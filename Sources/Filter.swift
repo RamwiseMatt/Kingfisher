97fabe0481be2a9cdf04da58347344fc5c7aa4db
Mats <matsiej@gmail.com>
2017-07-16 21:36:38 +0200

    Swift 4

--- Sources/Filter.swift
+++ Sources/Filter.swift
@@ -37,71 +37,71 @@ public typealias Transformer = (CIImage) -> CIImage?
 
 /// Supply a filter to create an `ImageProcessor`.
 public protocol CIImageProcessor: ImageProcessor {
-    var filter: Filter { get }
+	var filter: Filter { get }
 }
 
 extension CIImageProcessor {
-    public func process(item: ImageProcessItem, options: KingfisherOptionsInfo) -> Image? {
-        switch item {
-        case .image(let image):
-            return image.kf.apply(filter)
-        case .data(_):
-            return (DefaultImageProcessor.default >> self).process(item: item, options: options)
-        }
-    }
+	public func process(item: ImageProcessItem, options: KingfisherOptionsInfo) -> Image? {
+		switch item {
+		case .image(let image):
+			return image.kf.apply(filter)
+		case .data(_):
+			return (DefaultImageProcessor.default >> self).process(item: item, options: options)
+		}
+	}
 }
 
 /// Wrapper for a `Transformer` of CIImage filters.
 public struct Filter {
-    
-    let transform: Transformer
-
-    public init(tranform: @escaping Transformer) {
-        self.transform = tranform
-    }
-    
-    /// Tint filter which will apply a tint color to images.
-    public static var tint: (Color) -> Filter = {
-        color in
-        Filter { input in
-            let colorFilter = CIFilter(name: "CIConstantColorGenerator")!
-            colorFilter.setValue(CIColor(color: color), forKey: kCIInputColorKey)
-            
-            let colorImage = colorFilter.outputImage
-            let filter = CIFilter(name: "CISourceOverCompositing")!
-            filter.setValue(colorImage, forKey: kCIInputImageKey)
-            filter.setValue(input, forKey: kCIInputBackgroundImageKey)
-            return filter.outputImage?.cropped(to: input.extent)
-        }
-    }
-    
-    public typealias ColorElement = (CGFloat, CGFloat, CGFloat, CGFloat)
-    
-    /// Color control filter which will apply color control change to images.
-    public static var colorControl: (ColorElement) -> Filter = { arg -> Filter in
-        let (brightness, contrast, saturation, inputEV) = arg
-        return Filter { input in
-            let paramsColor = [kCIInputBrightnessKey: brightness,
-                               kCIInputContrastKey: contrast,
-                               kCIInputSaturationKey: saturation]
-            
-            let blackAndWhite = input.applyingFilter("CIColorControls", parameters: paramsColor)
-            let paramsExposure = [kCIInputEVKey: inputEV]
-            return blackAndWhite.applyingFilter("CIExposureAdjust", parameters: paramsExposure)
-        }
-        
-    }
+	
+	let transform: Transformer
+	
+	public init(tranform: @escaping Transformer) {
+		self.transform = tranform
+	}
+	
+	/// Tint filter which will apply a tint color to images.
+	public static var tint: (Color) -> Filter = {
+		color in
+		Filter { input in
+			let colorFilter = CIFilter(name: "CIConstantColorGenerator")!
+			colorFilter.setValue(CIColor(color: color), forKey: kCIInputColorKey)
+			
+			let colorImage = colorFilter.outputImage
+			let filter = CIFilter(name: "CISourceOverCompositing")!
+			filter.setValue(colorImage, forKey: kCIInputImageKey)
+			filter.setValue(input, forKey: kCIInputBackgroundImageKey)
+			return filter.outputImage?.cropping(to: input.extent)
+		}
+	}
+	
+	public typealias ColorElement = (CGFloat, CGFloat, CGFloat, CGFloat)
+	
+	/// Color control filter which will apply color control change to images.
+	public static var colorControl: (ColorElement) -> Filter = { arg -> Filter in
+		let (brightness, contrast, saturation, inputEV) = arg
+		return Filter { input in
+			let paramsColor = [kCIInputBrightnessKey: brightness,
+			                   kCIInputContrastKey: contrast,
+			                   kCIInputSaturationKey: saturation]
+			
+			let blackAndWhite = input.applyingFilter("CIColorControls", withInputParameters: paramsColor)
+			let paramsExposure = [kCIInputEVKey: inputEV]
+			return blackAndWhite.applyingFilter("CIExposureAdjust", withInputParameters: paramsExposure)
+		}
+		
+	}
 }
 
 extension Kingfisher where Base: Image {
-    /// Apply a `Filter` containing `CIImage` transformer to `self`.
-    ///
-    /// - parameter filter: The filter used to transform `self`.
-    ///
-    /// - returns: A transformed image by input `Filter`.
-    ///
-    /// - Note: Only CG-based images are supported. If any error happens during transforming, `self` will be returned.
-    public func apply(_ filter: Filter) -> Image {
+	/// Apply a `Filter` containing `CIImage` transformer to `self`.
+	///
+	/// - parameter filter: The filter used to transform `self`.
+	///
+	/// - returns: A transformed image by input `Filter`.
+	///
+	/// - Note: Only CG-based images are supported. If any error happens during transforming, `self` will be returned.
+	public func apply(_ filter: Filter) -> Image {
         
         guard let cgImage = cgImage else {
             assertionFailure("[Kingfisher] Tint image only works for CG-based image.")
@@ -143,3 +143,4 @@ public extension Image {
         return kf.apply(filter)
     }
 }
+
